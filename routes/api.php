8ab1d932ec4ff28b5191f17d7d72f6fc7171a531
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2025-12-08 10:39:59 +0600

    add auth routes and tasks resource rout

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('tasks', TaskController::class);
+});
